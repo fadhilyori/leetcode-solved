b9c8a2e3f527a06b261732fa9e1fd54ab5dbee42
Fadhil Yori <fadhil4812@gmail.com>
2023-07-16 00:49:50 +0700

    Time: 2135 ms (51.66%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -9,13 +9,16 @@ FROM
 JOIN 
   Department AS d ON e.DepartmentId = d.Id
 WHERE 
-  (SELECT 
-    COUNT(DISTINCT Salary)
-  FROM 
-    Employee AS e2
-  WHERE 
-    e2.Salary >= e.Salary
-    AND e2.DepartmentId = e.DepartmentId) <= 3
+  (
+    SELECT 
+      COUNT(DISTINCT Salary)
+    FROM 
+      Employee AS e2
+    WHERE 
+      e2.DepartmentId = e.DepartmentId
+      AND e2.Salary >= e.Salary
+  ) <= 3
 ORDER BY 
   Department, 
   Salary DESC;
+
