25cbd0c30c7d55a742c7a4d9457d31796ace8a20
Fadhil Yori <fadhil4812@gmail.com>
2023-07-16 22:53:41 +0700

    Time: 53 ms (49.27%), Space: 16.4 MB (43.51%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,31 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        
+        if not s:
+            return 0
+        
+        i = 0
+        N = len(s)
+        sign = 1
+        result = 0
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+        
+        # Handle the sign if present
+        if i < N and (s[i] == '-' or s[i] == '+'):
+            sign = -1 if s[i] == '-' else 1
+            i += 1
+            
+        # Convert remaining digits
+        while i < N and s[i].isdigit():
+            digit = int(s[i])
+
+            # Check for overflow
+            if result > INT_MAX // 10 or (result == INT_MAX // 10 and digit > INT_MAX % 10):
+                return INT_MAX if sign == 1 else INT_MIN
+
+            result = result * 10 + digit
+            i += 1
+
+        return sign * result
