e3487eef705f0e5b606bba3018c2a19ddc35c78d
Fadhil Yori <fadhil4812@gmail.com>
2023-07-27 22:51:04 +0700

    Time: 1631 ms (49.40%), Space: 30.3 MB (77.11%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        def check_feasibility(target):
-            extra = sum(min(power, target) for power in batteries)
-            return extra // n >= target
-        
-        left, right = 1, sum(batteries) // n + 1
+        total_batteries = sum(batteries)
+        left, right = 1, total_batteries // n + 1
         
         while left < right:
             mid = left + (right - left) // 2
             
-            if check_feasibility(mid):
+            extra = sum(min(power, mid) for power in batteries)
+            
+            if extra // n >= mid:
                 left = mid + 1
             else:
                 right = mid
