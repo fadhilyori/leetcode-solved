f24614b9606d4ee1ecac5af2d7bc7510259edea7
Fadhil Yori <fadhil4812@gmail.com>
2023-08-18 00:25:40 +0700

    Time: 2 ms (74.02%), Space: 2.8 MB (75.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,16 +1,30 @@
 func isPalindrome(s string) bool {
-    nonAlphanumericRegex := regexp.MustCompile("[^a-zA-Z0-9]+")
-    s = strings.ToLower(nonAlphanumericRegex.ReplaceAllString(s, ""))
-    l, r := 0, len(s) - 1
-    
-    for l < r {
-        if s[l] != s[r] {
-            return false
-        }
+	s = strings.ToLower(s)
+	left, right := 0, len(s)-1
+
+	for left < right {
+        sl, sr := rune(s[left]), rune(s[right])
+		if !isAlphanumeric(sl) {
+			left++
+			continue
+		}
+
+		if !isAlphanumeric(sr) {
+			right--
+			continue
+		}
+
+		if sl != sr {
+			return false
+		}
         
-        l++
-        r--
-    }
-    
-    return true
+		left++
+		right--
+	}
+
+	return true
+}
+
+func isAlphanumeric(c rune) bool {
+    return unicode.IsLetter(c) || unicode.IsNumber(c)
 }
